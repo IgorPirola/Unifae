3687318e678f4568a877ae31312f41ebe0912ea0
Igor M. Pirola <igormarianopirola@gmail.com>
2025-03-14 21:24:18 -0300

    :tada: initial commit

---
+++ 5o Sem/PP/TarifadorEstacionamento/src/main/java/com/mycompany/tarifadorestacionamento/TarifadorEstacionamento.java
@@ -0,0 +1,12 @@
+package com.mycompany.tarifadorestacionamento;
+
+/**
+ *
+ * @author igorp
+ */
+public class TarifadorEstacionamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
