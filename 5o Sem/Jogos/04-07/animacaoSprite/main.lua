5185df4b991cf422687cac141583eb5513c733d5
Igor M. Pirola <igormarianopirola@gmail.com>
2025-04-14 22:15:52 -0300

    ✨ feat
    motion animation

--- 5o Sem/Jogos/04-07/animacaoSprite/main.lua
+++ 5o Sem/Jogos/04-07/animacaoSprite/main.lua
@@ -16,7 +16,7 @@ local tempo = 0.1
 function love.load()
     LG.setBackgroundColor(1,1,1)
     personagem.imgdir = LG.newImage('sprite-dir.png')
-    personagem.imgesq = LG.newImage('sprite-dir.png')
+    personagem.imgesq = LG.newImage('sprite-esq.png')
     personagem.x = 100
     personagem.y = 200
 
@@ -29,5 +29,42 @@ function love.load()
 end
 
 function love.draw()
-    LG.draw(personagem.imgdir, quadros[direcao][passo], personagem.x, personagem.y)
+    LG.draw(padrao and personagem.imgdir or personagem.imgesq, quadros[direcao][passo], personagem.x, personagem.y)
+end
+
+function love.update(dt)
+    if not status then
+        tempo = tempo + dt
+        if tempo > 0.2 then
+            tempo = 0.1
+            -- Animação do Quad até o limite de imagens (8)
+            passo = passo + 1
+            if direcao == 'right' then
+                personagem.x = personagem.x + 5
+            elseif direcao == 'left' then
+                personagem.x = personagem.x - 5
+            end
+            -- Verificação do máximo Quad
+            if passo > max then
+                passo = 1
+            end
+        end
+    end    
+end
+
+function love.keypressed(tecla)
+    if quadros[tecla] then
+        direcao = tecla 
+        status = false
+    end
+    padrao = (tecla == 'right')
+end
+
+function love.keyreleased(tecla)
+    if quadros[tecla] and direcao == tecla then
+        status = true
+        passo = 1
+        direcao = 'right'
+        padrao = true
+    end
 end
