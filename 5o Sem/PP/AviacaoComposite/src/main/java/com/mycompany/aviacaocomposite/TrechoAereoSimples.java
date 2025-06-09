11e5548192b486105e8d4116b61f5537596a3cf3
Igor M. Pirola <igormarianopirola@gmail.com>
2025-06-09 20:58:23 -0300

    :tada: Initial Commit
    Study of the Composite Design Pattern

---
+++ 5o Sem/PP/AviacaoComposite/src/main/java/com/mycompany/aviacaocomposite/TrechoAereoSimples.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aviacaocomposite;
+
+/**
+ *
+ * @author igorp
+ */
+public class TrechoAereoSimples implements TrechoAereo {
+    private String origem;
+    private String destino;
+    private int custo;
+    private int distancia;
+    
+    public TrechoAereoSimples(String origem, String destino, int custo, int distancia){
+        super();
+        
+    }
+}
